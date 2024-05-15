d6ea28b5f43d2e43e0f92537a2a4b348987a84eb
eikatkoffbmstu <147157589+eikatkoffbmstu@users.noreply.github.com>
2024-05-16 02:09:24 +0300

    Update test_facade.cpp

--- tests/test_facade.cpp
+++ tests/test_facade.cpp
@@ -1,35 +1,78 @@
-// test.cpp
-
 #include <iostream>
 
-// Функция для сравнения двух чисел
-bool isEqual(int a, int b) {
-    return a == b;
+class SubsystemA
+{
+public:
+  void suboperation() { std::cout << "Subsystem A method" << std::endl; }
+};
+
+class SubsystemB
+{
+public:
+  void suboperation() { std::cout << "Subsystem B method" << std::endl; }
+};
+
+class SubsystemC
+{
+public:
+  void suboperation() { std::cout << "Subsystem C method" << std::endl; }
+};
+
+class Facade
+{
+public:
+  Facade() : subsystemA(new SubsystemA()), subsystemB(new SubsystemB()), subsystemC(new SubsystemC()) {}
+  ~Facade()
+  {
+    delete subsystemA;
+    delete subsystemB;
+    delete subsystemC;
+  }
+
+  void operation1()
+  {
+    subsystemA->suboperation();
+    subsystemB->suboperation();
+  }
+
+  void operation2()
+  {
+    subsystemC->suboperation();
+  }
+
+private:
+  SubsystemA *subsystemA;
+  SubsystemB *subsystemB;
+  SubsystemC *subsystemC;
+};
+
+// Функция для проверки работы метода operation1()
+void testOperation1()
+{
+  std::cout << "Running Operation 1 test..." << std::endl;
+  Facade facade;
+  facade.operation1(); // Вызов метода, который должен вызвать методы suboperation() у подсистем A и B
+  std::cout << "Operation 1 test completed" << std::endl;
 }
 
-// Функция для выполнения тестов
-void runTests() {
-    int x = 5;
-    int y = 5;
-    std::cout << "Test 1: ";
-    if (isEqual(x, y)) {
-        std::cout << "Passed" << std::endl;
-    } else {
-        std::cout << "Failed" << std::endl;
-    }
-
-    int z = 10;
-    std::cout << "Test 2: ";
-    if (!isEqual(x, z)) {
-        std::cout << "Passed" << std::endl;
-    } else {
-        std::cout << "Failed" << std::endl;
-    }
+// Функция для проверки работы метода operation2()
+void testOperation2()
+{
+  std::cout << "Running Operation 2 test..." << std::endl;
+  Facade facade;
+  facade.operation2(); // Вызов метода, который должен вызвать метод suboperation() у подсистемы C
+  std::cout << "Operation 2 test completed" << std::endl;
 }
 
-int main() {
-    // Запуск тестов
-    runTests();
+// Функция для запуска всех тестов
+void runTests()
+{
+  testOperation1();
+  testOperation2();
+}
 
-    return 0;
+int main()
+{
+  runTests(); // Запуск всех тестов
+  return 0;
 }
